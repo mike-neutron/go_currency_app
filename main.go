53afb189198c136ba3c76e8d8658731a27ed9158
mike <mike-neutron@yandex.ru>
2023-06-16 00:21:14 +0700

    main methods

--- main.go
+++ main.go
@@ -9,5 +9,17 @@ func main() {
         return c.SendString("Hello, World 👋!")
     })
 
+	app.Get("/api/stocks/current", func(c *fiber.Ctx) error {
+		return c.SendString("GET /api/stocks/current method")
+	})
+
+	app.Get("/api/stocks/date/date", func(c *fiber.Ctx) error {
+		return c.SendString("GET /api/stocks/date/date method")
+	})
+
+	app.Get("/api/stocks/exchange/USD/RUB", func(c *fiber.Ctx) error {
+		return c.SendString("GET /api/stocks/exchange/USD/RUB method")
+	})
+
     app.Listen(":8080")
 }
