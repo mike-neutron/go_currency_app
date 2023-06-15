2a3757f9f9d3254d37f5676e62a6e4cd2bcd901c
mike <mike-neutron@yandex.ru>
2023-06-16 00:34:10 +0700

    add params to methods

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
     app := fiber.New()
@@ -13,12 +15,12 @@ func main() {
 		return c.SendString("GET /api/stocks/current method")
 	})
 
-	app.Get("/api/stocks/date/date", func(c *fiber.Ctx) error {
-		return c.SendString("GET /api/stocks/date/date method")
+	app.Get("/api/stocks/date/:date", func(c *fiber.Ctx) error {
+		return c.SendString(c.Params("date"))
 	})
 
-	app.Get("/api/stocks/exchange/USD/RUB", func(c *fiber.Ctx) error {
-		return c.SendString("GET /api/stocks/exchange/USD/RUB method")
+	app.Get("/api/stocks/exchange/:from/:to", func(c *fiber.Ctx) error {
+		return c.SendString("from: "+c.Params("from")+ ", to: "+c.Params("to"))
 	})
 
     app.Listen(":8080")
